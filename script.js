708a323b38c3c95d81e616b8bd5f05e1f1534226
Gaymard <33988017+Erffa@users.noreply.github.com>
2019-07-19 00:11:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-
 window.onload = () => {
     console.log("onload");
-    window.addEventListener("deviceorientation", handleOrientation, true);
-}
-
-
-
-function handleOrientation(event) {
 
     const
     text = document.getElementById("text");
 
-    var 
-    absolute = event.absolute,
-    alpha    = event.alpha,
-    beta     = event.beta,
-    gamma    = event.gamma;
 
-    let msg = 
-    "absolute : " + absolute + "<br>" + 
-    "alpha : " + alpha + "<br>" + 
-    "beta : " + beta + "<br>" + 
-    "gamma : " + gamma + "<br>" + 
-    
-    console.log(msg);
-    text.innerHTML = msg;
+    try {
+        window.addEventListener(
+            "deviceorientation", 
+            (event) => {
+                var 
+                absolute = event.absolute,
+                alpha    = event.alpha,
+                beta     = event.beta,
+                gamma    = event.gamma;
+
+                let msg = 
+                "absolute : " + absolute + "<br>" + 
+                "alpha : " + alpha + "<br>" + 
+                "beta : " + beta + "<br>" + 
+                "gamma : " + gamma + "<br>" + 
+                
+                console.log(msg);
+            }, 
+            true);
+    }
+    catch (err) {
+        text.innerHTML = "error deviceorientation";
+    }
 }
