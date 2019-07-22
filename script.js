33c53e53cfae856df44e3d02b2095205733e0a3b
Gaymard <33988017+Erffa@users.noreply.github.com>
2019-07-22 14:53:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 window.onload = () => {
-    
-    console.log("onload");
 
     const
     button = document.getElementById('button'),
-    text = document.getElementById("text");
+    text1 = document.getElementById("text1"),
+    text2 = document.getElementById("text2");
 
     button.onclick = () => {
+        
+        text1.innerHTML = window.orientation;
+        
         var variable = undefined;
         
         window.addEventListener( 'deviceorientation', (event) => { variable = event.alpha; } , true );
         
         if (variable === undefined) {
-            text.innerHTML = "error : maybe orientation doesn't work";
+            text2.innerHTML = "error : maybe orientation doesn't work";
         }
         else {
-            text.innerHTML = "result : " + variable;
+            text2.innerHTML = "result : " + variable;
         }
     };
 }
